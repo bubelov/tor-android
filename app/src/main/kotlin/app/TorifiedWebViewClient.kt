c72772ddebae855f828f9d006ebd911fe91e8e81
Igor Bubelov <igor@bubelov.com>
2022-06-12 16:34:05 +0700

    Adjust HTTP timeouts

--- app/src/main/kotlin/app/TorifiedWebViewClient.kt
+++ app/src/main/kotlin/app/TorifiedWebViewClient.kt
@@ -8,16 +8,25 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.net.InetSocketAddress
 import java.net.Proxy
+import java.time.Duration
 
 class TorifiedWebViewClient : WebViewClient() {
+
+    private val httpClient by lazy {
+        val proxy = Proxy(Proxy.Type.SOCKS, InetSocketAddress("localhost", 9050))
+        OkHttpClient.Builder()
+            .proxy(proxy)
+            .connectTimeout(Duration.ofSeconds(60))
+            .readTimeout(Duration.ofSeconds(60))
+            .writeTimeout(Duration.ofSeconds(60))
+            .build()
+    }
 
     override fun shouldInterceptRequest(
         view: WebView?,
         request: WebResourceRequest,
     ): WebResourceResponse {
-        val proxy = Proxy(Proxy.Type.SOCKS, InetSocketAddress("localhost", 9050))
-        val client = OkHttpClient.Builder().proxy(proxy).build()
-        val call = client.newCall(Request.Builder().url(request.url.toString()).build())
+        val call = httpClient.newCall(Request.Builder().url(request.url.toString()).build())
         val response = call.execute()
         val body = response.body!!
 
